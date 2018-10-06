43590a13a07098fb67e38f72281727d2117cfe16
Márcio <marciodepaula133@gmail.com>
2018-10-06 14:20:13 -0300

    Unit test for calc service

--- src/app/_services/calc-rate.service.spec.ts
+++ src/app/_services/calc-rate.service.spec.ts
@@ -12,4 +12,17 @@ describe('CalcRateService', () => {
   it('should be created', inject([CalcRateService], (service: CalcRateService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('calcRate should return 0 on result and 38 on resultNoPlan ', inject([CalcRateService], (service: CalcRateService) => {
+    const data = {
+      duration: 20,
+      plan: 'FaleMais 30',
+      origin: '011',
+      destiny: '016'
+    };
+    const finalResult = service.calcRate(data);
+
+    expect(finalResult.result).toBe(0);
+    expect(finalResult.resultNoPlan).toBe(38);
+  }));
 });
