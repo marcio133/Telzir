755588d88f1f98be3b7f9781e2f1275356feebd2
Márcio <marciodepaula133@gmail.com>
2018-10-06 10:48:26 -0300

    Implemented routing navigation
    Added routing module and created home component, to implement routing navigation in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,11 @@
-import { CalcRateService } from './_services/calc-rate.service';
-import { Component, OnInit } from '@angular/core';
-import { ddds, plans } from './_utils/constants';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { StorageService } from './_services/storage.service';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 
-export class AppComponent implements OnInit {
-  ddds = ddds;
-  plans = plans;
-  form: FormGroup;
-  result: number;
-  resultNoPlan: number;
-  errorMessage: string = null;
-  constructor(private calcService: CalcRateService, private storageService: StorageService) { }
+export class AppComponent {
 
-  ngOnInit(): void {
-    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    // Add 'implements OnInit' to the class.
-    this.createForm();
-  }
-
-  createForm() {
-    this.form = new FormGroup({
-      origin: new FormControl(null, Validators.required),
-      destiny: new FormControl(null, Validators.required),
-      duration: new FormControl(null, Validators.compose(
-        [Validators.min(1), Validators.required])),
-      plan: new FormControl(null, Validators.required)
-    });
-  }
-
-  onSubmit() {
-    if (this.form.get('origin').value !== this.form.get('destiny').value) {
-      const resultService = this.calcService.calcRate(this.form.value);
-      this.result = resultService.result;
-      this.resultNoPlan = resultService.resultNoPlan;
-      this.storageService.saveQuery(this.form.value).subscribe(res=>{
-        console.log(res);
-      })
-    }else{
-      this.errorMessage = "Os ddds de origem e destino precisam ser diferentes."
-    }
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { StorageService } from './_services/storage.service';
 import { CalcRateService } from './_services/calc-rate.service';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, {useHash: true})
   ],
   providers: [CalcRateService, StorageService],
   bootstrap: [AppComponent]
